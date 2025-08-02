6638e7f1e259f649fdf7926e704d435fad09ecc2
Eric deQuevedo <ericdequevedo@gmail.com>
2025-08-01 19:25:12 -0700

    ya

--- data/projectsData.js
+++ data/projectsData.js
@@ -9,35 +9,35 @@ const projectsData = [
     title: 'Quantum Learning',
     description:
       'A learning platform for students to learn about quantum computing and Other Computer Science topics',
-    url: 'https://quantumlearn.vercel.app/',
+    url: 'https://robotric.org/Classroom',
     imgSrc: 'https://www.quantumcybersolutions.com/entanglement.webp',
   },
   {
     title: 'Ballru',
     description:
       'An application that connects atheletes with trainers, recuits coaches for local programs',
-    url: 'https://quantumlearn.vercel.app/',
+    url: 'https://robotric.org/',
     imgSrc: 'https://www.quantumcybersolutions.com/entanglement.webp',
   },
   {
     title: 'Kinetic Gyms',
     description:
       'The Idea That the work down in the gym can be converted to energy or electricity, imagine if your energy bill was running high, and you could go to a gym and work off steam, and bills.',
-    url: 'https://quantumlearn.vercel.app/',
+    url: 'https://robotric.org/',
     imgSrc: 'https://www.quantumcybersolutions.com/entanglement.webp',
   },
   {
     title: 'pickup',
     description:
       'An application that tracks teams for pickup games forms tournaments, and brackets so your team doesnt get skipped waiting to get on the court.',
-    url: 'https://www.quantumcybersolutions.com/',
+    url: 'https://robotric.org/',
     imgSrc: 'https://www.quantumcybersolutions.com/entanglement.webp',
   },
   {
     title: 'OchoView',
     description:
       'A platform that allows anyone to commentate games and makes it super easy to switch commentators, allowing for local people, college students get experience commentating sports games, like a stream ',
-    url: 'https://www.quantumcybersolutions.com/',
+    url: 'https://robotric.org/',
     imgSrc: 'https://rikara.vercel.app/2.webp',
   },
 
